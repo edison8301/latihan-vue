d1c4b44c8fc2e438556fbcb73ab9219e97ac4bb6
Thomas Alfa Edison <thomas.alfa.edison@gmail.com>
2020-09-05 10:12:28 +0700

    'update'

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,10 +1,16 @@
 <html>
 <head>
     <title>Latihan Vue - @yield('title')</title>
+    <?php /*
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-    <script src="{{ asset('/js/app.js') }}" type="text/javascript"></script>
+    */ ?>
+
 </head>
 <body>
     @yield('content')
+
+    <?php /*
+    <script src="{{ asset('/js/app.js') }}" type="text/javascript"></script>
+    */ ?>
 </body>
 </html>
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -4,8 +4,93 @@
 
 @section('content')
 
+
+
     <div id="app">
-        <h1>Hello World</h1>
+        <div>
+        @{{ message }} @{{ nama }}<br/>
+
+            Imperaetive <div id="div-nama"></div>
+        </div>
+
+        <div>
+            <input v-model="nama" id="tes">
+        </div>
+
+        <div>&nbsp;</div>
+
+        <div>
+            <input v-model="kegiatanBaru">
+            <button onclick="app.tambahKegiatan()">Tambah</button>
+        </div>
+
+        <div>
+            Daftar Kegiatan
+            <ol>
+                <li v-for="kegiatan in daftarKegiatan">
+                    @{{ kegiatan }}
+                </li>
+            </ol>
+
+        </div>
+
+
+        <div>
+
+        </div>
+
+        <v-btn color="primary">Tes</v-btn>
+
+        <daftar-lampiran></daftar-lampiran>
+
+        <example-component></example-component>
+
+
     </div>
+
+    <?php /*
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+
+    <script>
+        Vue.component('todo-item', {
+            props: ['tes'],
+            template: '<li>This is a todo: @{{ tes }}</li>'
+        });
+
+        var app = new Vue({
+            el: '#app',
+            data: {
+                message: 'Hello World',
+                nama: 'Thomas',
+                kegiatanBaru: '',
+                daftarKegiatan: [
+                    'Kegiatan 1',
+                    'Kegiatan 2',
+                    'Kegiatan 3'
+                ]
+            },
+            methods: {
+                tambahKegiatan: function () {
+                    //alert(this.kegiatanBaru);
+                    this.daftarKegiatan.push(this.kegiatanBaru);
+                    this.kegiatanBaru = '';
+                }
+            }
+        });
+
+        document.getElementById('tes').onkeyup = function () {
+            let isi = document.getElementById('tes').value;
+            //alert(isi);
+            document.getElementById('div-nama').innerHTML = isi;
+        };
+
+
+    </script>
+    */ ?>
+
+    <script src="{{ asset('/js/app.js') }}" type="text/javascript"></script>
+
+
+
 
 @endsection
