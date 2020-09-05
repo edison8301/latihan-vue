81fd7f89b3c39ee29bdfd8f7eab8e8399abfa2dc
Thomas Alfa Edison <thomas.alfa.edison@gmail.com>
2020-09-05 07:48:27 +0700

    'update'

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Latihan Vue - @yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <script src="{{ asset('/js/app.js') }}" type="text/javascript"></script>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layouts.main')
+
+
+@section('content')
+
+    <div id="app">
+        <h1>Hello World</h1>
+    </div>
+
+@endsection
